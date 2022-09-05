d03db7241fddb3fd8ce5b2e5b30daedb78f67280
Tiago Fonseca <tiago@tiagopaiva.me>
2022-09-05 14:03:41 -0300

    test2

--- pages/loads/[loadId].tsx
+++ pages/loads/[loadId].tsx
@@ -10,6 +10,14 @@ export default function LoadDetailPage({ loadId }: { loadId?: string }) {
   return <div>Hi Load {loadId ?? "NOTHING"}</div>;
 }
 
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
+
 export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {
